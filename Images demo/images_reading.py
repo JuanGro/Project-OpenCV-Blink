5145580c78e49be88d08a1d1de0cb634bbfe8789
juangro <ing.juangro@gmail.com>
2018-11-16 21:54:33 -0600

    Add images reading demo

---
+++ Images demo/images_reading.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2 as cv
+
+# Get image in color
+# img = cv.imread('./action.jpg', cv.IMREAD_COLOR)
+# Get image in gray scale
+img = cv.imread('./action.jpg', cv.IMREAD_GRAYSCALE)
+# img = cv.imread('./action.jpg', cv.IMREAD_UNCHANGED)
+
+# Show image in window normal not in the image original dimensions
+cv.namedWindow('image', cv.WINDOW_NORMAL)
+
+# Show the image
+cv.imshow('image', img)
+
+# Wait infenitely if is zero
+k = cv.waitKey(0)
+
+# wait for ESC key to exit
+if k == 27:
+    cv.destroyAllWindows()
+# wait for 's' key to save and exit
+elif k == ord('s'):
+    # Save image
+    cv.imwrite('image_output.png', img)
+    cv.destroyAllWindows()
