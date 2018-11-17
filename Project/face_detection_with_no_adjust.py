afe98a89a7c134832257e3c6e02a2baa27479699
juangro <ing.juangro@gmail.com>
2018-11-17 12:31:14 -0600

    Hide prints

--- Project/face_detection_with_no_adjust.py
+++ Project/face_detection_with_no_adjust.py
@@ -36,8 +36,8 @@ while(True):
             # For each eye
             for (ex, ey, ew, eh) in eyes:
                 if (ey + ey + eh) / 2 < (y + y + h) / 6:
-                    print("face:", y, y + h, (y + y + h) / 2)
-                    print("eye:", ey, ey + eh, (ey + ey + eh) / 2)
+                    # print("face:", y, y + h, (y + y + h) / 2)
+                    # print("eye:", ey, ey + eh, (ey + ey + eh) / 2)
                     # Draw a green rectangle
                     opencv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
                     eyes_count += 1
