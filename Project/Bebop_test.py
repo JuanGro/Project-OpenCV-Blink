579f1f71c7bf86eebe0c7c3dea38d185525c6e9d
juangro <ing.juangro@gmail.com>
2018-11-17 12:34:02 -0600

    Add bebop test and change file name

---
+++ Project/Bebop_test.py
@@ -0,0 +1,105 @@
+﻿# -*- coding: utf-8 -*-
+"""
+SET DE INSTRUCCIONES BEBOP Y BEBOP2
+
+CREAR UN OBJETO:
+
+Bebop(drone_type="Bebop") / Bebop(drone_type="Bebop2")
+
+CONECTAR CON EL DRONE VIA WIFI:
+
+connect(num_retries) / ESPECIFICA EL NUMERO DE INTENTOS DE ESTABLECER COMUNUICACIÓN CON EL DRONE ANTES DE MANDAR UN ERROR
+
+disconnect() / DESCONECTA LA COMUNICACIÓN CON EL DRONE
+
+DESPEGUE Y ATERRIZAJE:
+
+safe_takeoff(timeout) / DESPEGA EL DRONE Y VERIFICA QUE SE HAYA REALIZADO. ESTABLECE UN TIMEOUT QUE EN CASO DE SOBREPASAR CANCELA LA OPERACIÓN
+
+safe_land(timeout) / ATERRIZA EL DRONE Y VERIFICA QUE SE HAYA REALIZADO. ESTABLECE UN TIMEOUT QUE EN CASO DE SOBREPASAR CANCELA LA OPERACIÓN
+
+MOVIMIENTO:
+
+fly_direct(roll, pitch, yaw, vertical_movement, duration) / MUEVE EL DRONE SEGUN LO INDICADO EN ROLL, PITCH, YAW, VERTICAL_MOVEMENT Y DURATION.
+                                                            LOS PRIMEROS 4 PARAMETROS SE INTRODUCEN EN PORCENTAJE DE SU MAXIMA VELOCIDAD desde -100 hasta 100
+                                                            Y LA DURACIÓN DEL MOVIMIENTO EN SEGUNDOS
+
+CONFIGURACIÓN:
+
+set_max_altitude(altitude)  / ESTABLECE LA ALTITUD MÁXIMA PERMITIDA PARA EL DRONE EN VALORES DESDE 0.5 Y HASTA 150 METROS
+
+set_max_tilt(tilt)   / ESTABLECE LA INCLINACIÓN MÁXIMA PARA LOS MOVIMIENTOS DE ROLL, PITCH, YAW. SE MIDE EN GRADOS Y VA DESDE
+                        5(MOVIMIENTO LENTO) HASTA 30 (MUY RÁPIDO)
+
+set_max_vertical_speed(speed)  / ESTABLECE LA VELOCIDAD MÁXIMA VERTICAL EN METROS/SEGUNDO VA DESDE 0.5 HASTA 2.5
+
+REVISAR LOS SENSORES DEL DRONE:
+
+ask_for_state_update()  / ENVÍA LA SOLICITUD AL DRONE PARA QUE DEVUELVA LA INFORMACIÓN ACTUALIZADA DE LOS SENSORES
+
+bebop.sensors.SENSOR   / DEVUELVE LA INFORMACIÓN DEL SENSOR INDICADO
+POR EJEMPLO:
+bebop.sensors.flying_state
+bebop.sensors.battery
+
+
+"""
+from pyparrot.Bebop import Bebop #Carga la libreria del drone
+import sys, os
+
+# Method to implement the drone work
+def drone_functionality():
+    print("connecting")
+    success = bebop.connect(10) #Verifica la conexión
+    print(success)
+
+    # No cambiar los valores de las siguientes 3 configuraciones !!!!!!
+    bebop.set_max_altitude(2)           #Establece la altitud máxima en 3 metros
+    bebop.set_max_tilt(5)               #Establece la velocidad máxima de movimientos angulares en 5°
+    bebop.set_max_vertical_speed(0.5)   #Establece la velocidad máxima vertical en 0.5 m/s
+
+    if (success):
+        print("sleeping")
+        bebop.smart_sleep(4)            #Realiza una pausa en el programa
+        bebop.safe_takeoff(10)          #Despega el drone
+        bebop.ask_for_state_update()    #Actualiza la información de los sensores
+        print("flying state is %s" % bebop.sensors.flying_state) #imprime el estado del drone
+        bebop.smart_sleep(4)            #El drone se mantiene 4 segundos en el aire
+        bebop.fly_direct(0,0,-65,0,5)
+        bebop.ask_for_state_update()    #Actualiza la información de los sensores
+        print("flying state is %s" % bebop.sensors.flying_state) #imprime el estado del drone
+        bebop.smart_sleep(6)            #El drone se mantiene 4 segundos en el aire
+        bebop.fly_direct(0,35,0,0,12)
+        bebop.ask_for_state_update()    #Actualiza la información de los sensores
+        print("flying state is %s" % bebop.sensors.flying_state) #imprime el estado del drone
+        bebop.smart_sleep(13)            #El drone se mantiene 4 segundos en el aire
+        bebop.safe_land(10)             #Aterriza el drone
+        bebop.ask_for_state_update()    #Actualiza la información de los sensores
+        print("flying state is %s" % bebop.sensors.flying_state) #imprime el estado del drone
+        print("Battery is %s" % bebop.sensors.battery)  #imprime el estado de la bateria
+        print("DONE - disconnecting")
+        bebop.disconnect()              #Termina la conexión con el drone
+
+# MAIN!!!! This is what will be executed
+if __name__ == '__main__':
+    # Define the bebop
+    bebop = Bebop(drone_type="Bebop2") #Se crea el objeto bebop
+    # If no emergency call, execute this (control-c)
+    try:
+        drone_functionality()
+    # If emergency call
+    except KeyboardInterrupt:
+        # Shown in terminal the emergency
+        print('Interrupted')
+        try:
+            # Wait two seconds
+            bebop.smart_sleep(2)
+            # Land
+            bebop.safe_land(10)
+            sys.exit(0)
+        except SystemExit:
+            # Wait two seconds
+            bebop.smart_sleep(2)
+            # Land
+            bebop.safe_land(10)
+            os._exit(0)
--- Project/face_detection.py
+++ Project/face_detection.py
@@ -8,16 +8,15 @@ eye_cascade = opencv.CascadeClassifier('haarcascade_eye.xml')
 # Start camera capturing
 cap = opencv.VideoCapture(0)
 
-single_blink = 0
-double_blink = 0
+blink_count_time = 0
 
 while(True):
     # Capture frame-by-frame
-    ret, img = cap.read()
+    ret, video = cap.read()
 
     if (ret == 1):
         # Video format
-        gray = opencv.cvtColor(img, opencv.COLOR_BGR2GRAY)
+        gray = opencv.cvtColor(video, opencv.COLOR_BGR2GRAY)
 
         # Detect faces
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -25,41 +24,39 @@ while(True):
         # For each face
         for (x, y, w, h) in faces:
             # Draw a black rectangle
-            img = opencv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            video = opencv.rectangle(video, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
             roi_gray = gray[y : y + h, x : x + w]
-            roi_color = img[y : y + h, x : x + w]
+            roi_color = video[y : y + h, x : x + w]
 
             eyes = eye_cascade.detectMultiScale(roi_gray)
 
-            # If detect a blink
-            if len(eyes) >= 2:
-                single_blink = 0
-                double_blink = 0
-            elif len(eyes) == 1:
-                single_blink += 1
-                double_blink = 0
-            elif len(eyes) == 0:
-                single_blink = 0
-                double_blink += 1
-
-            if single_blink >= 2:
-                font = opencv.FONT_HERSHEY_SIMPLEX
-                opencv.putText(img, 'BLINK', (500, 700), font, 4, (255,255,255), 2, opencv.LINE_AA)
-            # elif double_blink >= 3:
-            #     font = opencv.FONT_HERSHEY_SIMPLEX
-            #     opencv.putText(img, 'DOUBLE BLINK', (150, 700), font, 4, (255,255,255), 2, opencv.LINE_AA)
+            eyes_count = 0
 
             # For each eye
             for (ex, ey, ew, eh) in eyes:
-                # Draw a green rectangle
-                opencv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+                if (ey + ey + eh) / 2 < (y + y + h) / 6:
+                    # print("face:", y, y + h, (y + y + h) / 2)
+                    # print("eye:", ey, ey + eh, (ey + ey + eh) / 2)
+                    # Draw a green rectangle
+                    opencv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+                    eyes_count += 1
+
+            # If detect a blink
+            if eyes_count == 1:
+                blink_count_time += 1
+            else:
+                blink_count_time = 0
+
+            if blink_count_time > 3:
+                font = opencv.FONT_HERSHEY_SIMPLEX
+                opencv.putText(video, 'BLINK', (500, 700), font, 4, (255,255,255), 2, opencv.LINE_AA)
 
         # Set the window size
         opencv.namedWindow('frame', opencv.WINDOW_NORMAL)
 
         # Display the resulting frame
-        opencv.imshow('frame', img)
+        opencv.imshow('frame', video)
 
         # Exit
         if opencv.waitKey(1) & 0xFF == ord('q'):
