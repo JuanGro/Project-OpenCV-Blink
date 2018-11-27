4f8c50b8a56e043fb194357d36ae008c88fb23aa
juangro <ing.juangro@gmail.com>
2018-11-27 12:55:47 -0600

    Remove tutorial code

--- Images/image_correction.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2 as cv
-img = cv.imread('./again_noise.jpg')
-mask = cv.imread('mask2.png',0)
-dst = cv.inpaint(img,mask,3,cv.INPAINT_TELEA)
-cv.imshow('dst',dst)
-cv.waitKey(0)
-cv.destroyAllWindows()
